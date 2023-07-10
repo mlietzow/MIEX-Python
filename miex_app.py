ec8f98419e3673182f0faca8258ee15118afdf6f
mlietzow <mlietzow@astrophysik.uni-kiel.de>
2023-07-10 16:19:16 +0200

    added option to download results

--- miex_app.py
+++ miex_app.py
@@ -79,7 +79,16 @@ with col2:
 
 st.divider()
 
-if st.button('Run'):
+col1, col2 = st.columns(2)
+with col1:
+    run_miex = st.button('Run')
+with col2:
+    plot_res = st.checkbox('Plot results', value=False)
+
+if run_miex:
+    #---------------------------------------------------------------------------------------------------
+    # 2. Read data files & Prepare the calculations
+    #---------------------------------------------------------------------------------------------------
     wavelength = np.zeros(nlam)
     ri_real = np.zeros((ncomp, nlam))
     ri_imag = np.zeros((ncomp, nlam))
@@ -119,11 +128,7 @@ if st.button('Run'):
         ri_imag[icomp] = k[:nlam]
         # first two lines of file give information about the content of the file; no need to save in a variable
 
-    placeholder = st.empty()
-    with placeholder.container():
-        progress_text = 'Calculating optical properties ...'
-        progress_bar = st.progress(0, text=progress_text)
-
+    # define radial step width
     radminlog = np.log10(radmin)
     radmaxlog = np.log10(radmax)
     if nrad > 1:
@@ -131,7 +136,14 @@ if st.button('Run'):
     else:
         steplog = 0.0
 
+    #---------------------------------------------------------------------------------------------------
+    # 3. Run the Mie scattering routines
+    #---------------------------------------------------------------------------------------------------
     counter = 0
+    placeholder = st.empty()
+    with placeholder.container():
+        progress_text = 'Calculating optical properties ...'
+        progress_bar = st.progress(0, text=progress_text)
     for ilam in range(nlam):
         weisum = 0.0
         wrad = 0.0
@@ -209,100 +221,176 @@ if st.button('Run'):
         albedo[ilam] = c_sca[ilam] / c_ext[ilam]
         g_sca[ilam] /= wqsc
 
-    placeholder.info('Plotting results ...')
-
-    data_dict = {
-        'wavelength': wavelength,
-        'Qext': q_ext,
-        'Qabs': q_abs,
-        'Qsca': q_sca,
-        'Qbk': q_bk,
-        'Qpr': q_ext - g_sca * q_sca,
-        'A': albedo,
-        'gsca': g_sca,
-    }
-
-    if nlam == 1:
-        df = pd.DataFrame(data_dict)
-        st.dataframe(
-            df, use_container_width=True, hide_index=True,
-            column_config={key: st.column_config.NumberColumn(format='%e') for key in data_dict},
-            )
-    else:
-        fig, ax = plt.subplots(2, 1, sharex=True, layout='constrained')
-
-        ax[0].plot(wavelength, q_ext, label='extinction')
-        ax[0].plot(wavelength, q_abs, label='absorption')
-        ax[0].plot(wavelength, q_sca, label='scattering')
-        ax[0].plot(wavelength, q_bk, label='backscattering')
-        ax[0].plot(wavelength, q_ext - g_sca * q_sca, label='radiation pressure')
-
-        ax[0].set_ylabel('efficiency factor')
-        ax[0].set_yscale('log')
-        ax[0].legend()
-
-        ax[1].plot(wavelength, albedo, label='single scattering albedo')
-        ax[1].plot(wavelength, q_sca, label='scattering assymetry factor')
-        
-        # ax[1].set_yscale('log')
-        ax[1].set_xlabel('wavelength [micron]')
-        ax[1].set_xscale('log')
-        ax[1].legend()
-
-        st.pyplot(fig, use_container_width=True)
-
+    #---------------------------------------------------------------------------------------------------
+    # 4. Save the results
+    #---------------------------------------------------------------------------------------------------
+    output_file = '# *** PROJECT PARAMETERS ***\n'
+    output_file += '#\n'
+    output_file += f'# Number of wavelengths            : {nlam}\n'
+    output_file += f'# Number of chemical components    : {ncomp}\n'
+    output_file += '# Relative abundances [%]          :\n'
+    for icomp in range(ncomp):
+        output_file += f'# {icomp+1}. component: {abun[icomp]*100}\n'
+    output_file += '# Name(s) of the dust data file(s) :\n'
+    for icomp in range(ncomp):
+        output_file += f'# {icomp+1}. component: {fnames[icomp].name}\n'
+    output_file += f'# Minimum grain radius [micron]    : {radmin}\n'
+    output_file += f'# Maximum grain radius [micron]    : {radmax}\n'
+    output_file += f'# Size distribution exponent       : {exponent}\n'
+    output_file += f'# Number of size bins              : {nrad}\n'
     if doSA:
+        output_file += f'# Number of scattering angles      : {nang2}\n'
+    output_file += '#\n'
+    output_file += '#\n'
+    #-----------------------------------------------------------------------------------------------
+    output_file += '# *** RESULTS ***\n'
+    output_file += '#\n'
+    output_file += '# 1. Wavelength [micron]         Q_ext                    C_ext [m^2]\n'
+    for ilam in range(nlam):
+        output_file += f'{wavelength[ilam]} {q_ext[ilam]} {c_ext[ilam]}\n'
+    output_file += '# =============================================================== #\n'
+
+    output_file += '# 2. Wavelength [micron]         Q_sca                    C_sca [m^2]\n'
+    for ilam in range(nlam):
+        output_file += f'{wavelength[ilam]} {q_sca[ilam]} {c_sca[ilam]}\n'
+    output_file += '# =============================================================== #\n'
+
+    output_file += '# 3. Wavelength [micron]         Q_bk                      C_bk [m^2]\n'
+    for ilam in range(nlam):
+        output_file += f'{wavelength[ilam]} {q_bk[ilam]} {c_bk[ilam]}\n'
+    output_file += '# =============================================================== #\n'
+
+    output_file += '# 4. Wavelength [micron]         Q_abs                    C_abs [m^2]\n'
+    for ilam in range(nlam):
+        output_file += f'{wavelength[ilam]} {q_abs[ilam]} {c_abs[ilam]}\n'
+    output_file += '# =============================================================== #\n'
+
+    output_file += '# 5. Wavelength [micron], Albedo\n'
+    for ilam in range(nlam):
+        output_file += f'{wavelength[ilam]} {albedo[ilam]}\n'
+    output_file += '# =============================================================== #\n'
+
+    output_file += '# 6. Wavelength [micron], Scattering asymmetry factor g\n'
+    for ilam in range(nlam):
+        output_file += f'{wavelength[ilam]} {g_sca[ilam]}\n'
+    output_file += '# =============================================================== #\n'
+
+    output_file += '# 7. Wavelength [micron]         Q_pr\n'
+    for ilam in range(nlam):
+        output_file += f'{wavelength[ilam]} {q_ext[ilam] - g_sca[ilam] * q_sca[ilam]}\n'
+    output_file += '# =============================================================== #\n'
+
+    output_file += '# 8. Wavelength [micron]     theta [degree]         S11-S12-S33-S34\n'
+    if doSA:
+        for ilam in range(nlam):
+            for iang in range(nang2):
+                # angx: scattering angle [degree]
+                angx = iang * 180.0 / (nang2 - 1.0)
+                output_file += f'{wavelength[ilam]} {angx} {S11[iang,ilam]} {S12[iang,ilam]} {S33[iang,ilam]} {S34[iang,ilam]}\n'
+    else:
+        output_file += '# not calculated.\n'
+        
+    st.download_button('Download results', output_file, file_name='results.txt')
+
+    #---------------------------------------------------------------------------------------------------
+    # 5. Plot the results
+    #---------------------------------------------------------------------------------------------------
+
+    if plot_res:
+        placeholder.info('Plotting results ...')
+
+        data_dict = {
+            'wavelength': wavelength,
+            'Qext': q_ext,
+            'Qabs': q_abs,
+            'Qsca': q_sca,
+            'Qbk': q_bk,
+            'Qpr': q_ext - g_sca * q_sca,
+            'A': albedo,
+            'gsca': g_sca,
+        }
+
         if nlam == 1:
-            fig, ax = plt.subplots(2, 2, sharex=True, layout='constrained')
-            theta = np.linspace(0, 180, nang2)
-
-            fig.suptitle(f'wavelength: {wavelength[0]} [micron]')
-
-            ax[0,0].plot(theta, S11[:,0] / S11[0,0])
-            ax[0,0].set_ylabel('S11 / S11(0 deg)')
-            ax[0,0].set_yscale('log')
-
-            ax[0,1].plot(theta, -S12[:,0] / S11[:,0])
-            ax[0,1].set_ylabel('-S12 / S11')
-            ax[0,1].yaxis.set_label_position("right")
-            ax[0,1].yaxis.tick_right()
-
-            ax[1,0].plot(theta, S33[:,0] / S11[:,0])
-            ax[1,0].set_xlabel('scattering angle [deg]')
-            ax[1,0].set_ylabel('S33 / S11')
-
-            ax[1,1].plot(theta, S34[:,0] / S11[:,0])
-            ax[1,1].set_xlabel('scattering angle [deg]')
-            ax[1,1].set_ylabel('S34 / S11')
-            ax[1,1].yaxis.set_label_position("right")
-            ax[1,1].yaxis.tick_right()
-
+            df = pd.DataFrame(data_dict)
+            st.dataframe(
+                df, use_container_width=True, hide_index=True,
+                column_config={key: st.column_config.NumberColumn(format='%e') for key in data_dict},
+                )
         else:
-            fig, ax = plt.subplots(2, 2, sharex=True, sharey=True, layout='constrained')
+            fig, ax = plt.subplots(2, 1, sharex=True, layout='constrained')
 
-            theta = np.linspace(0, 180, nang2)
+            ax[0].plot(wavelength, q_ext, label='extinction')
+            ax[0].plot(wavelength, q_abs, label='absorption')
+            ax[0].plot(wavelength, q_sca, label='scattering')
+            ax[0].plot(wavelength, q_bk, label='backscattering')
+            ax[0].plot(wavelength, q_ext - g_sca * q_sca, label='radiation pressure')
 
-            im = ax[0,0].pcolormesh(theta, wavelength, np.transpose(S11 / S11[0,:]), vmax=1, vmin=0, shading='nearest')
-            ax[0,0].set_title('S11 / S11(0 deg)')
-            ax[0,0].set_ylabel('wavelength [micron]')
-            ax[0,0].set_yscale('log')
-            fig.colorbar(im, ax=ax[0,0])
+            ax[0].set_ylabel('efficiency factor')
+            ax[0].set_yscale('log')
+            ax[0].legend()
 
-            im = ax[0,1].pcolormesh(theta, wavelength, np.transpose(-S12 / S11), vmin=-1, vmax=1, cmap='bwr')
-            ax[0,1].set_title('-S12 / S11')
-            fig.colorbar(im, ax=ax[0,1])
+            ax[1].plot(wavelength, albedo, label='single scattering albedo')
+            ax[1].plot(wavelength, q_sca, label='scattering assymetry factor')
+            
+            # ax[1].set_yscale('log')
+            ax[1].set_xlabel('wavelength [micron]')
+            ax[1].set_xscale('log')
+            ax[1].legend()
 
-            im = ax[1,0].pcolormesh(theta, wavelength, np.transpose(S33 / S11), vmin=-1, vmax=1, cmap='bwr')
-            ax[1,0].set_title('S33 / S11')
-            ax[1,0].set_xlabel('scattering angle [deg]')
-            ax[1,0].set_ylabel('wavelength [micron]')
-            fig.colorbar(im, ax=ax[1,0])
+            st.pyplot(fig, use_container_width=True)
 
-            im = ax[1,1].pcolormesh(theta, wavelength, np.transpose(S34 / S11), vmin=-1, vmax=1, cmap='bwr')
-            ax[1,1].set_title('S34 / S11')
-            ax[1,1].set_xlabel('scattering angle [deg]')
-            fig.colorbar(im, ax=ax[1,1])
+        if doSA:
+            if nlam == 1:
+                fig, ax = plt.subplots(2, 2, sharex=True, layout='constrained')
+                theta = np.linspace(0, 180, nang2)
 
-        st.pyplot(fig, use_container_width=True)
+                fig.suptitle(f'wavelength: {wavelength[0]} [micron]')
+
+                ax[0,0].plot(theta, S11[:,0] / S11[0,0])
+                ax[0,0].set_ylabel('S11 / S11(0 deg)')
+                ax[0,0].set_yscale('log')
+
+                ax[0,1].plot(theta, -S12[:,0] / S11[:,0])
+                ax[0,1].set_ylabel('-S12 / S11')
+                ax[0,1].yaxis.set_label_position("right")
+                ax[0,1].yaxis.tick_right()
+
+                ax[1,0].plot(theta, S33[:,0] / S11[:,0])
+                ax[1,0].set_xlabel('scattering angle [deg]')
+                ax[1,0].set_ylabel('S33 / S11')
+
+                ax[1,1].plot(theta, S34[:,0] / S11[:,0])
+                ax[1,1].set_xlabel('scattering angle [deg]')
+                ax[1,1].set_ylabel('S34 / S11')
+                ax[1,1].yaxis.set_label_position("right")
+                ax[1,1].yaxis.tick_right()
+
+            else:
+                fig, ax = plt.subplots(2, 2, sharex=True, sharey=True, layout='constrained')
+
+                theta = np.linspace(0, 180, nang2)
+
+                im = ax[0,0].pcolormesh(theta, wavelength, np.transpose(S11 / S11[0,:]), vmax=1, vmin=0, shading='nearest')
+                ax[0,0].set_title('S11 / S11(0 deg)')
+                ax[0,0].set_ylabel('wavelength [micron]')
+                ax[0,0].set_yscale('log')
+                fig.colorbar(im, ax=ax[0,0])
+
+                im = ax[0,1].pcolormesh(theta, wavelength, np.transpose(-S12 / S11), vmin=-1, vmax=1, cmap='bwr')
+                ax[0,1].set_title('-S12 / S11')
+                fig.colorbar(im, ax=ax[0,1])
+
+                im = ax[1,0].pcolormesh(theta, wavelength, np.transpose(S33 / S11), vmin=-1, vmax=1, cmap='bwr')
+                ax[1,0].set_title('S33 / S11')
+                ax[1,0].set_xlabel('scattering angle [deg]')
+                ax[1,0].set_ylabel('wavelength [micron]')
+                fig.colorbar(im, ax=ax[1,0])
+
+                im = ax[1,1].pcolormesh(theta, wavelength, np.transpose(S34 / S11), vmin=-1, vmax=1, cmap='bwr')
+                ax[1,1].set_title('S34 / S11')
+                ax[1,1].set_xlabel('scattering angle [deg]')
+                fig.colorbar(im, ax=ax[1,1])
+
+            st.pyplot(fig, use_container_width=True)
 
     placeholder.success('Done!')
